29ae15799becb7aea528870cb6661117321716a0
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-09-13 22:16:53 +0300

    11. Macros. Excercise 11.04 monitoring macro example

--- src/A01.clj
+++ src/A01.clj
@@ -851,3 +851,72 @@ alphabet
 (define-html-tags "h1" "h2")
 
 (h1 "hi" "ho")
+
+; Ex 11.04 Monitoring functions
+
+; iteration 1
+(defmacro defmonitored
+  [fn-name tx-fn & args-and-body]
+  (let [
+        ; TODO compile time bindings
+        ]
+    `(defn ~fn-name ~[]
+       ; TODO
+       )))
+
+; Testing
+(defmonitored my-func identity [])
+(my-func)
+
+; iteration 2
+
+(defmacro defmonitored
+  [fn-name tx-fn & args-and-body]
+  (let [pre-arg-list (take-while (complement sequential?) args-and-body)
+        fn-content (drop-while (complement sequential?) args-and-body)
+        fn-bodies (if (vector? (first fn-content))
+                    `(~fn-content)
+                    fn-content)]
+    `(defn ~fn-name ~@pre-arg-list
+       ; TODO
+       )))
+
+; iteration 3
+
+(defn wrap-fn-body
+  [fn-name tx-fn b]
+  (let [arg-list (first b)
+        fn-body (rest b)]
+    (when-not (first (filter #(= 'client-id %) arg-list))
+      (throw (ex-info "Missing client-id argument" {})))
+    `(~arg-list
+       (let [start-time# (System/nanoTime)]
+         (try
+           (let [result# (do ~fn-body)]
+             (~tx-fn {:name ~(name fn-name)
+                      :client-id ~'client-id
+                      :status :complete
+                      :start-time start-time#
+                      :end-time (System/nanoTime)})
+             result#)
+           (catch Exception e#
+             (~tx-fn {:name ~(name fn-name)
+                      :client-id ~'client-id
+                      :status :error
+                      :start-time start-time#
+                      :end-time (System/nanoTime)})
+             (throw e#)))))))
+
+(defmacro defmonitored
+  [fn-name tx-fn & args-and-body]
+  (let [pre-arg-list (take-while (complement sequential?) args-and-body)
+        fn-content (drop-while (complement sequential?) args-and-body)
+        fn-bodies (if (vector? (first fn-content))
+                    `(~fn-content)
+                    fn-content)]
+    `(defn ~fn-name ~@pre-arg-list
+       ~@(map (partial wrap-fn-body fn-name tx-fn) fn-bodies))))
+
+; Testing
+(defmonitored my-func println [client-id m]  (assoc m :client client-id))
+(my-func 32 {:data 123})
