ba5ed325194cd49a1423e90bd5a9d9c079bedb13
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-09-26 13:28:59 +0300

    5. Many to One: Reducing

--- src/A01.clj
+++ src/A01.clj
@@ -1065,6 +1065,36 @@ alphabet
 ; Ex 4.12: Parsing CSV with semantic-csv
 ; -> tennis.clj
 
+ ; 5. Many to One: Reducing
+(def weather-days
+  [{:max 31
+    :min 27
+    :description :sunny
+    :date "2019-09-24"}
+   {:max 28
+    :min 25
+    :description :cloudy
+    :date "2019-09-25"}
+   {:max 22
+    :min 18
+    :description :rainy
+    :date "2019-09-26"}
+   {:max 35
+    :min 16
+    :description :stormy
+    :date "2019-09-27"}
+   {:max 35
+    :min 19
+    :description :sunny
+    :date "2019-09-28"}])
+
+(apply max (map :max weather-days))
+(reduce (fn [max-day-so-far this-day]
+          (if (> (:max this-day) (:max max-day-so-far))
+            this-day
+            max-day-so-far))
+  weather-days)
+
 
 ; 11. Macros
 (defmacro minimal-macro []
