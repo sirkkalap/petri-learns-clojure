a7508d56f9f8b3ba80629c07f25bb040ba5f4fdc
Petri Sirkkala <petri.sirkkala@solita.fi>
2022-10-04 00:44:45 +0300

    Summarizing with Groupby and more

--- src/tennis_reduce.clj
+++ src/tennis_reduce.clj
@@ -1,4 +1,38 @@
-(ns tennis-reduce)
+(ns tennis-reduce
+  (:require
+    [clojure.java.io :as io]
+    [clojure.data.csv :as csv]
+    [semantic-csv.core :as sc]))
+
+(defn tennis-csv->tournament-groups [csv]
+  (with-open [r (io/reader csv)]
+    (->> (csv/read-csv r)
+      sc/mappify
+      (map #(select-keys % [:tourney_slug :winner_name :loser_name]))
+      (group-by :tourney_slug))))
+
+
+(defn tennis-csv->tournament-match-counts [csv]
+  (with-open [r (io/reader csv)]
+    (->> (csv/read-csv r)
+      sc/mappify
+      (group-by :tourney_slug)
+      (map (fn [[k ms]] [k (count ms)]))
+      (into {}))))
+
+(defn tennis-csv->tournament-match-counts [csv]
+  (with-open [r (io/reader csv)]
+    (->> (csv/read-csv r)
+      sc/mappify
+      ;;....
+      )))
+
+(defn tennis-csv->tournament-match-counts [csv]
+  (with-open [r (io/reader csv)]
+    (->> (csv/read-csv r)
+      sc/mappify
+      (map #(select-keys % [:tourney_slug :winner_name :loser_name]))
+      (group-by :tourney_slug))))
 
 (defn streak-string [current-wins current-losses]
   (cond (pos? current-wins) (str "Won " current-wins)
@@ -22,3 +56,24 @@
        :current-wins 0
        :current-losses 0}
       matches)))
+
+; Testing
+; (in-ns 'tennis-reduce)
+(def tournaments (tennis-csv->tournament-match-counts "match_scores_1991-2016_unindexed_csv.csv"))
+(sort (keys tournaments))
+(take 5 (get tournaments "chicago"))
+(count (get tournaments "chicago"))
+
+(defn tennis-csv->tournament-match-counts [csv]
+  (with-open [r (io/reader csv)]
+    (->> (csv/read-csv r)
+      sc/mappify
+      (group-by :tourney_slug)
+      (map (fn [[k ms]] [k (count ms)]))
+      (into {}))))
+(def tournament-totals (tennis-csv->tournament-match-counts "match_scores_1991-2016_unindexed_csv.csv"))
+(get tournament-totals "chicago")
+(get tournament-totals "wimbledon")
+(get tournament-totals "roland-garros")
+
+; Summarizing Tennis Scores
