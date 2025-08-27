fe92c9197490ee6de102e8d3ad31d90b2e1ab400
Petri Sirkkala <petri.sirkkala@solita.fi>
2025-08-28 01:41:34 +0300

    kondo

--- hello-world/src/hello_world/core.clj
+++ hello-world/src/hello_world/core.clj
@@ -1,10 +1,10 @@
 (ns hello-world.core
-    (:gen-class))
+  (:gen-class))
 
 (defn greet [name]
-      (str "Moikka, " name "!"))
+  (str "Moikka, " name "!"))
 
 (defn -main [& args]
-      ;; Jos annetaan nimi komentoriviltä, käytetään sitä, muuten "maailma"
-      (let [target (or (first args) "maailma")]
-           (println (greet target))))
+  (let [args (or args [])
+        target (or (first args) "maailma")]
+     println (greet target)))
